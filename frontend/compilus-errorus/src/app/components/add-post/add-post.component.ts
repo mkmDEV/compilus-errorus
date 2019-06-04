010644df559bee22987cc53321fe6a1d5afd5212
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-06-04 15:43:04 +0200

    "Frontend of uploading picture."

---
+++ frontend/compilus-errorus/src/app/components/add-post/add-post.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
+
+@Component({
+  selector: 'app-add-post',
+  templateUrl: './add-post.component.html',
+  styleUrls: ['./add-post.component.css']
+})
+export class AddPostComponent implements OnInit {
+  selectedFile: File
+
+  constructor(private postsService:PostsService) { }
+
+  ngOnInit() {
+  }
+
+  onFileChanged(event:any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUpload() {
+    this.postsService.uploadImage(this.selectedFile).subscribe();
+  }
+}
