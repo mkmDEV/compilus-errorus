dcd2cc47233eb6c7c889b9d08220e247e9817c82
cskata <katalin.csortos@gmail.com>
2019-06-04 20:29:45 +0200

    Minor corrections suggested by IDE

--- frontend/compilus-errorus/src/app/services/posts.service.ts
+++ frontend/compilus-errorus/src/app/services/posts.service.ts
@@ -1,21 +1,22 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpEvent, HttpRequest } from '@angular/common/http';
-import { Post } from '../models/Post';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders, HttpEvent, HttpRequest} from '@angular/common/http';
+import {Post} from '../models/Post';
 
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json'
   })
-} 
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
-  postsUrl:string = 'http://localhost:8080/posts';
+  postsUrl = 'http://localhost:8080/posts';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(this.postsUrl);
@@ -24,11 +25,11 @@ export class PostsService {
   savePost() {
 
   }
-  
-  uploadImage(file:File): Observable<HttpEvent<{}>> {
-    let formdata: FormData = new FormData();
-    formdata.append('file', file);
-    const req = new HttpRequest('POST', 'http://localhost:8080/upload', formdata);
+
+  uploadImage(file: File): Observable<HttpEvent<{}>> {
+    const formData: FormData = new FormData();
+    formData.append('file', file);
+    const req = new HttpRequest('POST', 'http://localhost:8080/upload', formData);
     return this.http.request(req);
   }
 }
