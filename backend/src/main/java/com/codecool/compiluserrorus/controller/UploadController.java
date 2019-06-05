49bb8001b1c013aff807bbaea1a272adbc6e191f
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-06-05 09:11:40 +0200

    Picture upload changed.

--- backend/src/main/java/com/codecool/compiluserrorus/controller/UploadController.java
+++ backend/src/main/java/com/codecool/compiluserrorus/controller/UploadController.java
@@ -1,9 +1,6 @@
 package com.codecool.compiluserrorus.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -16,11 +13,12 @@ import java.nio.file.Paths;
 public class UploadController {
     private static final String UPLOADED_FOLDER = System.getenv("IMAGE_PATH");
     @PostMapping("/upload")
-    public void saveImage(@RequestParam("file") MultipartFile file) {
+    public void saveImage(@RequestPart("file") MultipartFile file) {
         try {
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            Files.write(path, bytes);
+            //byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER);
+            Files.copy(file.getInputStream(), path.resolve(file.getOriginalFilename()));
+            //Files.write(path, bytes);
         } catch (IOException e) {
             e.printStackTrace();
         }
