114058c870203610cc7a769b0301e0074924784a
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-06-17 23:26:00 +0200

    Components restructured.

--- frontend/compilus-errorus/src/app/app.module.ts
+++ frontend/compilus-errorus/src/app/app.module.ts
@@ -6,15 +6,28 @@ import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {PostsComponent} from './components/posts/posts.component';
-import {PostItemComponent} from './components/post-item/post-item.component';
+import {PostItemComponent} from './components/posts/post-list/post-item/post-item.component';
 import {ContainerComponent} from './components/container/container.component';
 import {LeftComponent} from './components/left/left.component';
-import {RightComponent} from './components/right/right.component';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
 import {HeaderComponent} from './components/header/header.component';
-import {AddPostComponent} from './components/add-post/add-post.component';
+import {AddPostComponent} from './components/posts/add-post/add-post.component';
 import { EventsComponent } from './components/events/events.component';
-import { AdvertComponent } from './components/advert/advert.component';
+import { AdvertComponent } from './components/sidebar/advert/advert.component';
 import { GroupsComponent } from './components/groups/groups.component';
+import { CommentListComponent } from './components/posts/post-list/post-item/comment-list/comment-list.component';
+import { CommentItemComponent } from './components/posts/post-list/post-item/comment-list/comment-item/comment-item.component';
+import { PostListComponent } from './components/posts/post-list/post-list.component';
+import { AddCommentComponent } from './components/posts/post-list/post-item/comment-list/add-comment/add-comment.component';
+import { LatestEventsListComponent } from './components/sidebar/latest-events-list/latest-events-list.component';
+import { OwnGroupsListComponent } from './components/sidebar/own-groups-list/own-groups-list.component';
+import { EventListComponent } from './components/events/event-list/event-list.component';
+import { EventItemComponent } from './components/events/event-list/event-item/event-item.component';
+import { AddEventComponent } from './components/events/event-list/add-event/add-event.component';
+import { GroupListComponent } from './components/groups/group-list/group-list.component';
+import { GroupItemComponent } from './components/groups/group-list/group-item/group-item.component';
+import { AddGroupComponent } from './components/groups/group-list/add-group/add-group.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
     declarations: [
@@ -23,12 +36,25 @@ import { GroupsComponent } from './components/groups/groups.component';
         PostItemComponent,
         ContainerComponent,
         LeftComponent,
-        RightComponent,
+        SidebarComponent,
         HeaderComponent,
         AddPostComponent,
         EventsComponent,
         AdvertComponent,
-        GroupsComponent
+        GroupsComponent,
+        CommentListComponent,
+        CommentItemComponent,
+        PostListComponent,
+        AddCommentComponent,
+        LatestEventsListComponent,
+        OwnGroupsListComponent,
+        EventListComponent,
+        EventItemComponent,
+        AddEventComponent,
+        GroupListComponent,
+        GroupItemComponent,
+        AddGroupComponent,
+        ProfileComponent
     ],
     imports: [
         BrowserModule,
---
+++ frontend/compilus-errorus/src/app/components/sidebar/latest-events-list/latest-events-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-events-list',
+  templateUrl: './latest-events-list.component.html',
+  styleUrls: ['./latest-events-list.component.css']
+})
+export class LatestEventsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
