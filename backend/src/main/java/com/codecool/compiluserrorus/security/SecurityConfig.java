3f2e7ce9d9b379fa5c1d0e385ebfae6d6d06af4e
mkmDEV <mkmdev18@gmail.com>
2019-07-01 15:02:34 +0200

    + Implemented Spring Security

---
+++ backend/src/main/java/com/codecool/compiluserrorus/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.codecool.compiluserrorus.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/register", "/login").permitAll()
+                .antMatchers("/posts/**").authenticated()
+                .antMatchers("/comments/**").authenticated()
+                .antMatchers("/events/**").authenticated()
+                .antMatchers("/upload/**").authenticated()
+                .antMatchers(HttpMethod.GET, "/members/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/members/**").hasRole("ADMIN")
+                .anyRequest().denyAll()
+            .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
+    }
+}
