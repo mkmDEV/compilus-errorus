bb94045598391bc87b7c8bb9484856ac0cc9472f
mkmDEV <mkmdev18@gmail.com>
2019-07-01 15:01:46 +0200

    - Deleted User Repository because of member repo

--- backend/src/main/java/com/codecool/compiluserrorus/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.compiluserrorus.repository;
-
-import com.codecool.compiluserrorus.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<Member, Long> {
-}
