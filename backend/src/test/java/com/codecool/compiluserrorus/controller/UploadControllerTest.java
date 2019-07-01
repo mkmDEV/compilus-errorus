c9af8f40cc89621e9bc4df6cc85b8de20da98318
cskata <katalin.csortos@gmal.com>
2019-07-01 12:43:28 +0200

    Disabled security temporally

--- backend/src/test/java/com/codecool/compiluserrorus/controller/UploadControllerTest.java
+++ backend/src/test/java/com/codecool/compiluserrorus/controller/UploadControllerTest.java
@@ -3,16 +3,12 @@ package com.codecool.compiluserrorus.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -37,7 +33,6 @@ class UploadControllerTest {
 
     @Test
     @Order(1)
-    @WithMockUser
     void imATeapot() throws Exception {
 
         url = "/teapot";
