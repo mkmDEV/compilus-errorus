51fe0599f85a2cf070cb47c98cc6a10461723541
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-07-02 00:38:34 +0200

    CORS enabled.

--- backend/src/main/java/com/codecool/compiluserrorus/controller/AuthController.java
+++ backend/src/main/java/com/codecool/compiluserrorus/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
--- backend/src/main/java/com/codecool/compiluserrorus/controller/RegistrationController.java
+++ backend/src/main/java/com/codecool/compiluserrorus/controller/RegistrationController.java
@@ -5,11 +5,13 @@ import com.codecool.compiluserrorus.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class RegistrationController {
 
     private final MemberService memberService;
@@ -21,6 +23,7 @@ public class RegistrationController {
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody Member member) {
+        System.out.println("request received");
         memberService.register(member);
         return new ResponseEntity(HttpStatus.OK);
     }
