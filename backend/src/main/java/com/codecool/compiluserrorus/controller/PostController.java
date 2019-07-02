7692d3ef8d0cf9a07a2b683346f71694bb36f54b
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-07-02 15:41:01 +0200

    getLoggedInMemberPosts() parameter modified

--- backend/src/main/java/com/codecool/compiluserrorus/controller/PostController.java
+++ backend/src/main/java/com/codecool/compiluserrorus/controller/PostController.java
@@ -1,7 +1,7 @@
 package com.codecool.compiluserrorus.controller;
 
+import com.codecool.compiluserrorus.model.Member;
 import com.codecool.compiluserrorus.model.Post;
-import com.codecool.compiluserrorus.service.MemberService;
 import com.codecool.compiluserrorus.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -27,14 +27,13 @@ public class PostController {
     }
 
     @GetMapping("/logged-in-member")
-    public List<Post> getLoggedInMemberPosts() {
-        return postService.getLoggedInMemberPosts(1L);
+    public List<Post> getLoggedInMemberPosts(@RequestBody Member member) {
+        return postService.getLoggedInMemberPosts(member);
     }
 
     @PostMapping
     public Post addPost(@Valid @RequestBody Post post) {
-        postService.addPost(post, post.getMember());
-        return post;
+        return postService.addPost(post, post.getMember());
     }
 
     @PutMapping("/{id}")
