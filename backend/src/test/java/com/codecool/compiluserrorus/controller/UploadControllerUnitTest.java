6b678028f6bae866a5ee80e57ee6428fb3421e60
cskata <katalin.csortos@gmail.com>
2019-07-02 20:40:26 +0200

    Fixed imATeapot test

--- backend/src/test/java/com/codecool/compiluserrorus/controller/UploadControllerUnitTest.java
+++ backend/src/test/java/com/codecool/compiluserrorus/controller/UploadControllerUnitTest.java
@@ -1,5 +1,6 @@
 package com.codecool.compiluserrorus.controller;
 
+import com.codecool.compiluserrorus.service.PostService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
@@ -17,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
+@ComponentScan(basePackageClasses = {UploadController.class})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 @AutoConfigureMockMvc
