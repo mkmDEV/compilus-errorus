cbd48514dc0f6a8df3dfda8e0385ea05ad03324b
cskata <katalin.csortos@gmail.com>
2019-07-02 21:04:28 +0200

    Fixed finding logged in member by using its email

--- backend/src/main/java/com/codecool/compiluserrorus/service/PostService.java
+++ backend/src/main/java/com/codecool/compiluserrorus/service/PostService.java
@@ -7,6 +7,7 @@ import com.codecool.compiluserrorus.repository.PostRepository;
 import com.codecool.compiluserrorus.util.Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -14,14 +15,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
-    private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
     @Value("${IMAGE_PATH}")
     private String imagePath;
@@ -32,10 +32,15 @@ public class PostService {
         return posts;
     }
 
+    public Member findMemberByEmail(Member member) {
+        return this.memberRepository.findByEmail(member.getEmail()).orElse(null);
+    }
+
     public List<Post> getLoggedInMemberPosts(Member member) {
-        Member loggedInMember = memberService.getLoggedInMember(member);
+        Member loggedInMember = findMemberByEmail(member);
+
         if (loggedInMember != null) {
-            List<Post> posts = postRepository.getPostsByMemberIdOrderByPostingDateDesc(loggedInMember.getId());
+            List<Post> posts = postRepository.getPostsByMember_EmailOrderByPostingDateDesc(loggedInMember.getEmail());
             posts.forEach(post -> post.setRomanDate(Util.setRomanDate(post.getPostingDate())));
             return posts;
         }
@@ -43,9 +48,14 @@ public class PostService {
     }
 
     public Post addPost(Post post, Member member) {
-        Member postMember = memberService.getLoggedInMember(member);
+        Member postMember = findMemberByEmail(member);
         post.setMember(postMember);
-        postRepository.save(post);
+        try {
+            postRepository.save(post);
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            throw new DataIntegrityViolationException("Message cannot be null");
+        }
         return post;
     }
 
@@ -62,7 +72,7 @@ public class PostService {
         return amendPost;
     }
 
-    public void deletePost(Long id) {
+    public boolean deletePost(Long id) {
         Post postToDelete = postRepository.findById(id).orElse(null);
         if (postToDelete != null) {
             Path path = Paths.get(imagePath + postToDelete.getImage());
@@ -74,5 +84,6 @@ public class PostService {
             }
         }
         postRepository.findById(id).ifPresent(deletablePost -> postRepository.deleteById(id));
+        return true;
     }
 }
