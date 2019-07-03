682995c457bd6c33d5f69b027c995e8f987870c4
cskata <katalin.csortos@gmal.com>
2019-07-03 14:28:49 +0200

    Added RequiredArgsConstructor annotation to MemberController

--- backend/src/main/java/com/codecool/compiluserrorus/controller/MemberController.java
+++ backend/src/main/java/com/codecool/compiluserrorus/controller/MemberController.java
@@ -4,10 +4,8 @@ import com.codecool.compiluserrorus.model.Member;
 import com.codecool.compiluserrorus.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,11 +18,6 @@ import java.util.List;
 public class MemberController {
 
     private final MemberService memberService;
-
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
 
     @PostMapping
     public List<Member> getFriends( @RequestBody Member member) {
