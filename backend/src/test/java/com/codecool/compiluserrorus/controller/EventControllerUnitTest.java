b711c9070f5cb80418d690e8c0104a88a087ce82
cskata <katalin.csortos@gmal.com>
2019-07-04 15:25:08 +0200

    Tested getting ordered events when logged out and with no events

--- backend/src/test/java/com/codecool/compiluserrorus/controller/EventControllerUnitTest.java
+++ backend/src/test/java/com/codecool/compiluserrorus/controller/EventControllerUnitTest.java
@@ -4,7 +4,6 @@ import com.codecool.compiluserrorus.model.Event;
 import com.codecool.compiluserrorus.model.Member;
 import com.codecool.compiluserrorus.service.EventService;
 import com.codecool.compiluserrorus.service.MemberService;
-import com.codecool.compiluserrorus.service.PostService;
 import com.codecool.compiluserrorus.util.EventTestsUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
@@ -19,7 +18,8 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -70,6 +70,37 @@ class EventControllerUnitTest {
         assertEquals(actualResponseBody, objectMapper.writeValueAsString(this.testEvents));
 
         verify(this.eventService).getOrderedEvents();
+        verifyNoMoreInteractions(this.eventService);
+    }
+
+    @Test
+    @Order(2)
+    @WithMockUser
+    public void testWithoutEventsWithLoggedInUser() throws Exception {
+        when(this.eventService.getOrderedEvents()).thenReturn(null);
+
+        MvcResult mvcResult = this.mockMvc.perform(get(MAIN_URL))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String actualResponseBody = mvcResult.getResponse().getContentAsString();
+        assertTrue(actualResponseBody.isEmpty());
+
+        verify(this.eventService).getOrderedEvents();
+        verifyNoMoreInteractions(this.eventService);
+    }
+
+    @Test
+    @Order(3)
+    public void getEventsWithLoggedOutUser() throws Exception {
+        int numberOfEvents = 5;
+        this.testEvents = EventTestsUtil.getEvents(numberOfEvents);
+
+        when(this.eventService.getOrderedEvents()).thenReturn(this.testEvents);
+
+        this.mockMvc.perform(get(MAIN_URL))
+                .andExpect(status().isForbidden());
+
         verifyNoMoreInteractions(this.eventService);
     }
 }
