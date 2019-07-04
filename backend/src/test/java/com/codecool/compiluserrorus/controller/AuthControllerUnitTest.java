a56e6c7085348bad6d6fd7c659edafdf4aedd880
cskata <katalin.csortos@gmail.com>
2019-07-04 18:59:59 +0200

    Tested login with bad credentials

--- backend/src/test/java/com/codecool/compiluserrorus/controller/AuthControllerUnitTest.java
+++ backend/src/test/java/com/codecool/compiluserrorus/controller/AuthControllerUnitTest.java
@@ -2,13 +2,17 @@ package com.codecool.compiluserrorus.controller;
 
 import com.codecool.compiluserrorus.model.UserCredentials;
 import com.codecool.compiluserrorus.security.JwtTokenServices;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
@@ -16,47 +20,69 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AuthControllerUnitTest {
 
-    @MockBean
-    Authentication authentication;
+    private static final String EMAIL = "test@test.hu";
+    private static final String PASSWORD = "PASSWORD";
 
     @MockBean
-    AuthenticationManager authenticationManager;
+    private Authentication authentication;
 
     @MockBean
-    JwtTokenServices jwtTokenServices;
+    private AuthenticationManager authenticationManager;
+
+    @MockBean
+    private JwtTokenServices jwtTokenServices;
 
     @Autowired
-    AuthController authController;
+    private AuthController authController;
 
-    // TODO test failed login
     @Test
-    public void testLogin() {
-        String email = "test@test.hu";
-        String password = "password";
-
-        when(authentication.getAuthorities()).thenReturn(Collections.emptyList());
-        when(this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password)))
-                .thenReturn(authentication);
+    @Order(1)
+    public void testSuccessFulLogin() {
+        when(this.authentication.getAuthorities()).thenReturn(Collections.emptyList());
+        when(this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(EMAIL, PASSWORD)))
+                .thenReturn(this.authentication);
         String token = "token";
-        when(jwtTokenServices.createToken(email, Collections.emptyList())).thenReturn(token);
+        when(this.jwtTokenServices.createToken(EMAIL, Collections.emptyList())).thenReturn(token);
 
         UserCredentials data = UserCredentials.builder()
-                .email("test@test.hu")
-                .password("password")
+                .email(EMAIL)
+                .password(PASSWORD)
                 .build();
-        ResponseEntity result = authController.login(data);
+
+        ResponseEntity result = this.authController.login(data);
 
         assertEquals("{roles=[], email=test@test.hu, token=token}", result.getBody().toString());
-        verify(authentication).getAuthorities();
-        verify(authenticationManager).authenticate(new UsernamePasswordAuthenticationToken(email, password));
-        verify(jwtTokenServices).createToken(email, Collections.emptyList());
+
+        verify(this.authentication).getAuthorities();
+        verify(this.authenticationManager).authenticate(new UsernamePasswordAuthenticationToken(EMAIL, PASSWORD));
+        verify(this.jwtTokenServices).createToken(EMAIL, Collections.emptyList());
+    }
+
+    @Test
+    @Order(2)
+    public void testLoginWithInvalidCredentials() {
+        when(this.authentication.getAuthorities()).thenThrow(BadCredentialsException.class);
+        when(this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(EMAIL, PASSWORD)))
+                .thenReturn(this.authentication);
+
+        UserCredentials data = UserCredentials.builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .build();
+
+        assertThrows(BadCredentialsException.class, () -> this.authController.login(data));
+
+        verify(this.authentication).getAuthorities();
+        verify(this.authenticationManager).authenticate(new UsernamePasswordAuthenticationToken(EMAIL, PASSWORD));
     }
 }
