a01f2110f94a6d7be5ccd84f16f43f01c9194af5
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-07-04 10:32:39 +0200

    CommentService tests refactored.

--- backend/src/test/java/com/codecool/compiluserrorus/service/CommentServiceUnitTest.java
+++ backend/src/test/java/com/codecool/compiluserrorus/service/CommentServiceUnitTest.java
@@ -24,7 +24,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;;
+import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
 @ComponentScan(basePackageClasses = {CommentService.class})
@@ -45,18 +45,22 @@ public class CommentServiceUnitTest {
     private CommentService commentService;
 
     private Member testMember;
+    private Member loggedInMember;
     private Post testPost;
     private Comment testComment;
     private Comment updatedCommentData;
 
     @BeforeEach
-    void init() {
-        testMember = Member.builder()
+    public void init() {
+        this.testMember = Member.builder()
                 .name("Test Name")
                 .email("test@test.hu")
                 .password("password")
                 .build();
-        testPost = Post.builder()
+        this.loggedInMember = Member.builder()
+                .email("test@test.hu")
+                .build();
+        this.testPost = Post.builder()
                 .message("Test post message")
                 .postingDate(LocalDateTime.of(2019, 7, 3, 21, 16))
                 .likes(10)
@@ -64,7 +68,7 @@ public class CommentServiceUnitTest {
                 .postType(PostType.USER)
                 .member(testMember)
                 .build();
-        testComment = Comment.builder()
+        this.testComment = Comment.builder()
                 .message("Test comment message")
                 .postingDate(LocalDateTime.of(2019, 7, 3, 21, 18))
                 .likes(10)
@@ -72,7 +76,7 @@ public class CommentServiceUnitTest {
                 .post(testPost)
                 .member(testMember)
                 .build();
-        updatedCommentData = Comment.builder()
+        this.updatedCommentData = Comment.builder()
                 .message("Updated test comment message")
                 .likes(100)
                 .dislikes(100)
@@ -82,9 +86,11 @@ public class CommentServiceUnitTest {
     @ParameterizedTest
     @Order(1)
     @ValueSource(ints = {1, 5, 25, 100, 250, 500, 1000})
-    void testGetCommentsOrderedByDate(int comments) {
+    public void testGetCommentsOrderedByDate(int comments) {
         List<Comment> commentList = CommentTestUtil.getOrderedComments(comments);
-        when(commentRepository.getCommentsByPostIdOrderByDate(STUB_ID)).thenReturn(commentList);
+
+        when(this.commentRepository.getCommentsByPostIdOrderByDate(STUB_ID)).thenReturn(commentList);
+
         List<Comment> orderedComments = this.commentService.getCommentsOrderedByDate(STUB_ID);
 
         assertIterableEquals(commentList, orderedComments);
@@ -93,59 +99,53 @@ public class CommentServiceUnitTest {
 
     @Order(2)
     @Test
-    void testAddComment() {
-        Member loggedInMember = Member.builder()
-                .email("test@test.hu")
-                .build();
-        when(this.memberService.getLoggedInMember(loggedInMember)).thenReturn(this.testMember);
-        when(this.commentRepository.save(testComment)).thenReturn(testComment);
-        Comment newComment = this.commentService.addComment(testComment, loggedInMember);
+    public void testAddComment() {
+        when(this.memberService.getLoggedInMember(this.loggedInMember)).thenReturn(this.testMember);
+        when(this.commentRepository.save(this.testComment)).thenReturn(this.testComment);
 
-        assertEquals(testComment, newComment);
-        verify(this.memberService).getLoggedInMember(loggedInMember);
-        verify(this.commentRepository).save(testComment);
+        Comment newComment = this.commentService.addComment(this.testComment, this.loggedInMember);
+
+        assertEquals(this.testComment, newComment);
+        verify(this.memberService).getLoggedInMember(this.loggedInMember);
+        verify(this.commentRepository).save(this.testComment);
     }
 
     @Order(3)
     @Test
-    void testAddCommentThrowsExceptionWithoutMessage() {
-        Member loggedInMember = Member.builder()
-                .email("test@test.hu")
-                .build();
+    public void testAddCommentThrowsExceptionWithoutMessage() {
         Comment commentWithoutMessage = Comment.builder()
                 .postingDate(LocalDateTime.of(2019, 7, 3, 21, 18))
-                .post(testPost)
-                .member(testMember).build();
+                .post(this.testPost)
+                .member(this.testMember).build();
 
-        when(this.memberService.getLoggedInMember(loggedInMember)).thenReturn(this.testMember);
+        when(this.memberService.getLoggedInMember(this.loggedInMember)).thenReturn(this.testMember);
         when(this.commentRepository.save(commentWithoutMessage)).thenThrow(DataIntegrityViolationException.class);
 
-        assertThrows(DataIntegrityViolationException.class, () -> {
-               this.commentService.addComment(commentWithoutMessage, loggedInMember);
-        });
-        verify(this.memberService).getLoggedInMember(loggedInMember);
+        assertThrows(DataIntegrityViolationException.class, () ->
+               this.commentService.addComment(commentWithoutMessage, this.loggedInMember));
+        verify(this.memberService).getLoggedInMember(this.loggedInMember);
         verify(this.commentRepository).save(commentWithoutMessage);
     }
 
     @Order(4)
     @Test
-    void testUpdateComment() {
-        when(this.commentRepository.findById(STUB_ID)).thenReturn(Optional.ofNullable(testComment));
+    public void testUpdateComment() {
+        when(this.commentRepository.findById(STUB_ID)).thenReturn(Optional.ofNullable(this.testComment));
 
-        Comment updatedComment = this.commentService.updateComment(STUB_ID, updatedCommentData);
+        Comment updatedComment = this.commentService.updateComment(STUB_ID, this.updatedCommentData);
 
-        assertEquals(updatedCommentData.getMessage(), updatedComment.getMessage());
-        assertEquals(updatedCommentData.getLikes(), updatedComment.getLikes());
-        assertEquals(updatedCommentData.getDislikes(), updatedComment.getDislikes());
+        assertEquals(this.updatedCommentData.getMessage(), updatedComment.getMessage());
+        assertEquals(this.updatedCommentData.getLikes(), updatedComment.getLikes());
+        assertEquals(this.updatedCommentData.getDislikes(), updatedComment.getDislikes());
         verify(this.commentRepository).findById(STUB_ID);
     }
 
     @Order(4)
     @Test
-    void testUpdateNonExistingCommentReturnsNull() {
+    public void testUpdateNonExistingCommentReturnsNull() {
         when(this.commentRepository.findById(STUB_ID)).thenReturn(Optional.empty());
 
-        Comment updatedComment = this.commentService.updateComment(STUB_ID, updatedCommentData);
+        Comment updatedComment = this.commentService.updateComment(STUB_ID, this.updatedCommentData);
 
         assertNull(updatedComment);
         verify(this.commentRepository).findById(STUB_ID);
@@ -153,15 +153,15 @@ public class CommentServiceUnitTest {
 
     @Order(6)
     @Test
-    void testDeleteComment() {
-        when(this.commentRepository.findById(STUB_ID)).thenReturn(Optional.ofNullable(testComment));
+    public void testDeleteComment() {
+        when(this.commentRepository.findById(STUB_ID)).thenReturn(Optional.ofNullable(this.testComment));
         assertTrue( () -> this.commentService.deleteComment(STUB_ID));
         verify(this.commentRepository).deleteById(STUB_ID);
     }
 
     @Order(7)
     @Test
-    void testDeleteNonExistingComment() {
+    public void testDeleteNonExistingComment() {
         when(this.commentRepository.findById(STUB_ID)).thenReturn(Optional.empty());
         assertFalse( () -> this.commentService.deleteComment(STUB_ID));
     }
